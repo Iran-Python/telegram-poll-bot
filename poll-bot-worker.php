3bf25ba8563f5c6d65d7e4b73b25c6b2e7be30dd
Iran-Python <sh28safaei@gmail.com>
2016-04-07 13:31:32 +0430

    Update poll-bot-worker.php

--- poll-bot-worker.php
+++ poll-bot-worker.php
@@ -4,7 +4,7 @@ set_time_limit(0);
 
 require_once 'PollBot.php';
 
-define('BOT_TOKEN', 'XXXXXXXXX:XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX');
+define('BOT_TOKEN', '195980847:AAENfGy_AJCW4JY7D5H66q9hBqWRSQKaCBE');
 
 $bot = new PollBot(BOT_TOKEN, 'PollBotChat');
 $bot->runLongpoll();
